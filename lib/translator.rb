10e76ee160064a30042eb3d4d8b90b55139d5699
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-07-29 16:16:20 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,50 @@
-# require modules here
+require "yaml"
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file)
+  emotes = YAML.load_file(file)
+  emoticons = {}
+  emotes.each do |key, value| 
+    emoticons[key] = {}
+    emoticons[key][:japanese] = value[1]
+    emoticons[key][:english] = value[0]
+  end
+  emoticons
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon)
+ emoticons = load_library(file_path)
+ japanese_equiv = " "
+  match_emo = emoticons.keys.find do |k|
+    
+    emoticons[k][:english] == emoticon
+    japanese_equiv = emoticons[k][:japanese]
+  end
+  binding.pry
+ if match_emo
+   return emoticons[match_emo][:japanese]
+ else 
+   "Sorry, that emoticon was not found"
+  end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path, emoticon)
+  emoticons = load_library(file_path)
+ match = emoticons.keys.find do |k|
+  emoticons[k][:japanese] == emoticon
 end
+  if match != nil
+    return match
+    else "Sorry, that emoticon was not found"
+  #binding.pry
+   end
+end
+  #emoticons.keys.find do |k|
+    #emoticons[k][:english] == emoticon
+  
+  
+  
+  
+  
+  
+  return "Sorry, that emoticon was not found"
